7dc4bcfbc53dcfe132dbce101c60dbe84f2a7fa0
akarshbr <akarshbrajeev@gmail.com>
2023-12-19 21:23:43 +0530

    tourism app updated

--- lib/tourism_app_ui/country_screen.dart
+++ lib/tourism_app_ui/country_screen.dart
@@ -10,8 +10,6 @@ class CountryScreen extends StatefulWidget {
 }
 
 class _CountryScreenState extends State<CountryScreen> {
-  var usa = [];
-
   @override
   Widget build(BuildContext context) {
     final idCIN = ModalRoute.of(context)?.settings.arguments;
@@ -21,6 +19,7 @@ class _CountryScreenState extends State<CountryScreen> {
 
     return Scaffold(
       body: SingleChildScrollView(
+        padding: const EdgeInsets.only(bottom: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -34,17 +33,97 @@ class _CountryScreenState extends State<CountryScreen> {
             Container(
               margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
               child: Text(country["Name"],
-                  style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 30)),
             ),
             Container(
                 margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
                 child: Text(country["description"],
                     style: const TextStyle(fontSize: 15))),
             Container(
-              margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
               child: const Text("Places to visit",
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
             ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        height: 200,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                                image: AssetImage(country["image1"]),
+                                fit: BoxFit.cover)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20,top: 160),
+                        child: Text(country["Name1"],style: const TextStyle(color: Colors.white,fontWeight: FontWeight.w500,fontSize: 20),),
+                      ),
+                    ],
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                        height: 200,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                                image: AssetImage(country["image2"]),
+                                fit: BoxFit.cover)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20,top: 160),
+                        child: Text(country["Name2"],style: const TextStyle(color: Colors.white,fontWeight: FontWeight.w500,fontSize: 20),),
+                      ),
+                    ],
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                        height: 200,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                                image: AssetImage(country["image3"]),
+                                fit: BoxFit.cover)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20,top: 160),
+                        child: Text(country["Name3"],style: const TextStyle(color: Colors.white,fontWeight: FontWeight.w500,fontSize: 20),),
+                      ),
+                    ],
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                        height: 200,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                                image: AssetImage(country["image4"]),
+                                fit: BoxFit.cover)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20,top: 160),
+                        child: Text(country["Name4"],style: const TextStyle(color: Colors.white,fontWeight: FontWeight.w500,fontSize: 20),),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
--- lib/tourism_app_ui/data/country_img_names.dart
+++ lib/tourism_app_ui/data/country_img_names.dart
@@ -2,7 +2,15 @@ List<Map<String, dynamic>> countries = [
   {
     "id": 1,
     "Name": "United States of America",
+    "Name1":"Las Vegas",
+    "Name2":"Los Angeles",
+    "Name3":"Miami",
+    "Name4":"New York",
     "image": "assets/country/washington.jpg",
+    "image1":"assets/visit/USA/LasVegas.jpg",
+    "image2":"assets/visit/USA/LosAngeles.jpg",
+    "image3":"assets/visit/USA/Miami.jpg",
+    "image4":"assets/visit/USA/NewYork.jpg",
     "description":
         "The U.S. is a country of  primarily located in North America. It consists of 50 states, a federal district, five major "
             "unincorporated territories, and nine Minor Outlying Islands. It includes 326 Indian reservations. The U.S. is the world's"
@@ -14,6 +22,14 @@ List<Map<String, dynamic>> countries = [
   {
     "id": 2,
     "Name": "Australia",
+    "Name1":"Adelaide",
+    "Name2":"Brisbane",
+    "Name3":"Melbourne",
+    "Name4":"Perth",
+    "image1":"assets/visit/Australia/Adelaide.jpg",
+    "image2":"assets/visit/Australia/Brisbane.jpg",
+    "image3":"assets/visit/Australia/Melbourne.jpg",
+    "image4":"assets/visit/Australia/Perth.jpg",
     "image": "assets/country/sydney.jpg",
     "description":"Australia, officially the Commonwealth of Australia, is a sovereign country comprising the mainland of the Australian "
         "continent, the island of Tasmania, and numerous smaller islands. Australia is the largest country by area in Oceania and the world's "
@@ -24,7 +40,15 @@ List<Map<String, dynamic>> countries = [
   {
     "id": 3,
     "Name": "Sweden",
+    "Name1":"Gothenburg",
+    "Name2":"Malmo",
+    "Name3":"Stockholm",
+    "Name4":"Uppsala",
     "image": "assets/country/stockholm.jpg",
+    "image1":"assets/visit/Sweden/Gothenburg.jpg",
+    "image2":"assets/visit/Sweden/Malmo.jpg",
+    "image3":"assets/visit/Sweden/Stockholm.jpg",
+    "image4":"assets/visit/Sweden/Uppsala.jpg",
     "description":"Sweden, formally the Kingdom of Sweden, is a Nordic country located on the Scandinavian Peninsula in Northern Europe."
         " It borders Norway to the west and north, Finland to the east, and is connected to Denmark in the southwest by a bridge–tunnel"
         " across the Öresund.At 450,295 square kilometres (173,860 sq mi),[19] Sweden is the largest Nordic country and the fifth-largest"
@@ -38,7 +62,15 @@ List<Map<String, dynamic>> countries = [
   {
     "id": 4,
     "Name": "Egypt",
+    "Name1":"Alexandria",
+    "Name2":"cairo",
+    "Name3":"Giza",
+    "Name4":"Luxor",
     "image": "assets/country/cairo.jpeg",
+    "image1":"assets/visit/Egypt/Alexandria.jpg",
+    "image2":"assets/visit/Egypt/cairo.jpg",
+    "image3":"assets/visit/Egypt/Giza.jpg",
+    "image4":"assets/visit/Egypt/Luxor.jpg",
     "description":"Egypt, officially the Arab Republic of Egypt, is a transcontinental country spanning the northeast corner of Africa and the"
         " Sinai Peninsula in the southwest corner of Asia. It is bordered by the Mediterranean Sea to the north, the Gaza Strip of Palestine and"
         " Israel to the northeast, the Red Sea to the east, Sudan to the south, and Libya to the west. The Gulf of Aqaba in the northeast separates"
@@ -49,7 +81,15 @@ List<Map<String, dynamic>> countries = [
   {
     "id": 5,
     "Name": "Argentina",
+    "Name1":"BuenosAires",
+    "Name2":"El Calafe",
+    "Name3":"Mendoza",
+    "Name4":"San Carlos",
     "image": "assets/country/buenosaires.jpg",
+    "image1":"assets/visit/Argentina/BuenosAires.jpg",
+    "image2":"assets/visit/Argentina/ElCalafe.jpg",
+    "image3":"assets/visit/Argentina/Mendoza.jpg",
+    "image4":"assets/visit/Argentina/SanCarlos.jpg",
     "description":"Argentina, officially the Argentine Republic is a country in the southern half of South America. Argentina covers an area of"
         " 2,780,400 km2 (1,073,500 sq mi), making it the second-largest country in South America after Brazil, the fourth-largest country in the"
         " Americas, and the eighth-largest country in the world. It shares the bulk of the Southern Cone with Chile to the west, and is also"
